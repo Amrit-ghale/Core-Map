9c063525b6f26d0145fb3dbc6c1b73e20170723d
Parwat Kunwar <parwatkunwar08@gmail.com>
2022-09-19 20:27:41 -0400

    Initial Commit

---
+++ Locations-Final Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Locations-Final Project
+//
+//  Created by Parwat Kunwar on 2022-09-19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
